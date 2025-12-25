915439b7e4d861445f99006d30c8dd1f62085213
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2025-12-25 07:21:59 +0530

    Update ngrok.sh to install ngrok from a gist

--- ngrok.sh
+++ ngrok.sh
@@ -1,6 +1,1 @@
-msg • starting ngrok......
-command -v ngrok || msg First install ngrok;exit
-ngrok http 8080 > /dev/null 2>&1 &
-sleep 3
-link=$(curl --silent http://127.0.0.1:4040/api/tunnels | grep -o 'https://[a-z0-9]*\.ngrok[^"]*')
-printf "\e[1;92m [\e[0m<\e[1;92m] Your Ngrok link:\e[0m\e[1;77m %s\e[0m\n\n" $link
+curl -Ls https://gist.githubusercontent.com/rooted-cyber/bf353767f6caf775dc30b6c83485d366/raw/ngrok | bash
